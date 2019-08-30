e3f7a0e92d3447c8aaf7ad512006a42edfd021c9
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-08-30 13:41:33 -0700

    Added function to load current location

--- CompletedProjects/maps_project/js/crap_map.js
+++ CompletedProjects/maps_project/js/crap_map.js
@@ -2,7 +2,7 @@
 
 function initMap() {
 
-  // Map Options
+  // Map Options (initial coordinates for UC Irvine)
   var options = {
     zoom: 14,
     center: {lat: 33.6405, lng: -117.8443}
@@ -10,52 +10,85 @@ function initMap() {
 
   // New Map
   var map = new google.maps.Map(document.getElementById('map'), options);
+  var infoWindow = new google.maps.InfoWindow;
 
 
-  /*
-  // Add marker with custom icon
-  var marker = new google.maps.Marker({
-      map:map,
-      draggable: true,
-      icon: 'imgs/toilet.png',
-      animation: google.maps.Animation.DROP,
-      position:{lat:33.644096,lng: -117.8443}
-  });
-
-  // Infowindow when you click on Marker
-  var infoWindow = new google.maps.InfoWindow({
-      content: '<h3> Bathroom </h3>'
-  });
-
-  marker.addListener('click', function() {
-      infoWindow.open(map, marker)
-  })
-  */
-
-  addMarker({lat:33.644096,lng: -117.8443});
-  addMarker({lat:33.644096,lng: -117.8490});
-
-
-  // Function for adding Marker
-  function addMarker(coords){
+  // ––––––––– Function for adding Marker ––––––––
+  // The marker has a cusotm toilet icon, drop animation,
+  // coordinates,and name of the bathroom location
+  function addMarker(mker){
       var marker = new google.maps.Marker({
           map:map,
           draggable: true,
           icon: 'imgs/toilet.png',
           animation: google.maps.Animation.DROP,
-          position: coords
+          position: mker.coords
+      });
+
+      // Add title/name of bathroom
+      var infoWindow = new google.maps.InfoWindow({
+          content: mker.content
+      });
+
+      // Open info window when marker is clicked
+      marker.addListener('click', function(){
+          infoWindow.open(map, marker);
       });
   }
 
 
+  // Event listener to add markers wherever the user clicks on the map
+    google.maps.event.addListener(map, 'click',
+      function(event){
+          addMarker({coords:event.latLng});
+      })
 
 
-}
+  // –––––––– Get user location ––––––––
+  // It takes  it takes a while to load user's location, so be patient in case
+  // it doesn't load fast enough.
 
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
+  function getUserLocation(){
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
+  }
+
+
+  // Function calls
+  // getUserLocation();
+
+
 }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+
+
+
+
+//
