92c9e0cbfc479462c80b955e55b428992571d727
israeljimenez <israelj@uci.edu>
2019-09-12 12:26:05 -0700

    documented code

--- CompletedProjects/tic-tac-toe/js/main.js
+++ CompletedProjects/tic-tac-toe/js/main.js
@@ -1,9 +1,156 @@
-const boardSquares = []
-const resetButton = document.getElementById("reset-button")
-player = "x"
-running = true
-turn = 0
 
+//Board Squares will be stored in an array
+const boardSquares = []
+
+//We want to keep track of the current player
+var player = "x"
+
+//Keeps track of the number of turns taken
+var turn = 0
+
+//Want to know whenever the game is running
+var running = true
+
+//Each square on the board is an object of the BoardSquare class
+class BoardSquares {
+  constructor(element) {
+    this.element = element;
+    this.element.addEventListener("click", this, false);
+    this.match = false;
+    this.choice = null;
+    this.element.style.fontFamily = "Manjari"
+  }
+
+  //Restarts the game
+  reset() {
+    this.choice = null;
+    this.match = false
+    this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = ''
+    running = true;
+    player = "x";
+    document.getElementById("winner").innerHTML = "<br>";
+    document.getElementById("reset-button").disabled = true;
+    canvas.clearRect(0,0,document.getElementById("gameboard").clientHeight,document.getElementById("gameboard").offsetWidth);
+    document.getElementById("myCanvas").hidden = true;
+  }
+
+  //Checks every single possiblilty for a win whenever there have been more then 4 turn taken
+  //Calls drawLine function to draw a line
+  checkForWinner(){
+    if (turn >= 4) {
+      var x;
+
+      if (this.choice == "x") {
+        canvas.strokeStyle = "#E93636";
+      }
+
+      else {
+        canvas.strokeStyle = "#4C4CEE";
+      }
+
+      for (var i = 0;i<3; i++) {
+        x = i*3
+        if (boardSquares[x].choice ==
+          this.choice && boardSquares[x+1].choice == this.choice && boardSquares[x+2].choice == this.choice) {
+          this.match = true;
+          this.drawLine([.11,0],[.14,.333],[.89,0], [.14,.333],i)
+        }
+      }
+
+      for (var i = 0;i<3;i++) {
+        if (boardSquares[i].choice ==
+          this.choice && boardSquares[i+3].choice == this.choice && boardSquares[i+6].choice == this.choice) {
+          this.match = true;
+          this.drawLine([.168,.333],[.064,0],[.168,.333],[.89,0],i)
+        }
+      }
+
+      if (boardSquares[0].choice == this.choice && boardSquares[4].choice == this.choice && boardSquares[8].choice == this.choice){
+        this.match = true;
+        this.drawLine([.108,0],[.067,0],[.897,0],[.883,0],0)
+      }
+
+      else if (boardSquares[2].choice == this.choice && boardSquares[4].choice == this.choice && boardSquares[6].choice == this.choice){
+        this.match = true;
+        this.drawLine([.897,0],[.067,0],[.108,0],[.883,0],0)
+      }
+
+      if (this.match) {
+        document.getElementById("myCanvas").hidden = false;
+        document.getElementById("winner").innerHTML = "Winner is: " + player.toUpperCase();
+        document.getElementById("reset-button").disabled = false;
+        running = false;
+      }
+    }
+  }
+
+  //Checks whether the game ended in a draw
+  checkForDraw() {
+    if (turn == 8 && this.match == false) {
+      document.getElementById("winner").innerHTML = "The game ended in a draw";
+      document.getElementById("reset-button").disabled = false;
+      running = false;
+    }
+  }
+
+  //Handles all events that take place on each square
+  handleEvent(event) {
+    switch (event.type) {
+
+      //When player clicks on square, calls selectSquare function
+      case "click":
+      if (running) {
+        if (player == "x" && this.choice == null) {
+          this.selectSquare("x","#E93636","o");
+        }
+        else if (player == "o" && this.choice == null) {
+          this.selectSquare("o","#4C4CEE","x")
+        }
+      }
+    }
+  }
+
+  //Draws a line on canvas according to values passed in
+  drawLine(rightX,rightY,leftX,leftY,i){
+    canvas.beginPath();
+    canvas.moveTo(this.fracToPixel(rightX[0]+(i)*(rightX[1])),this.fracToPixel(rightY[0]+(i)*(rightY[1])));
+    canvas.lineTo(this.fracToPixel(leftX[0]+(i)*(leftX[1])),this.fracToPixel(leftY[0]+(i)*(leftY[1])));
+    canvas.stroke();
+  }
+
+  //Changes the text on the square
+  selectSquare(currPlayer, color,nxtPlayer ){
+    this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '<p>'+currPlayer.toUpperCase()+'</p>';
+    this.element.querySelector(".face-container").querySelector(".facedown").style.color = color;
+    this.choice = currPlayer;
+    this.checkForWinner();
+    this.checkForDraw();
+    player = nxtPlayer
+    turn ++;
+  }
+
+  //Converts fraction to pixel
+  fracToPixel(frac){
+    return frac*c.width
+  }
+}
+
+//event listener for the reset button
+document.getElementById("reset-button").addEventListener('click', () => {
+  resetGame();
+  turn = 0;
+});
+
+//called on by event listener for reset button
+//iterates thorought boardSquares array resetting each square one at a time
+function resetGame() {
+  boardSquares.forEach((square)=> {
+    square.reset()
+  });
+}
+
+//––––––––– Function for adding all the squares onto the Board ––––––––
+//Adds boardsquare divs as well as the canvas div following all squares
 function generateHTMLBoardSquares(){
   const boardElement = document.getElementById('gameboard');
   htmlChange = ''
@@ -20,162 +167,56 @@ function generateHTMLBoardSquares(){
 
 }
 
-class BoardSquares {
-  constructor(element,canvas) {
-    this.element = element;
-    this.element.addEventListener("click", this, false);
-    this.match = false;
-    this.choice = null;
-    this.canvas = canvas.getContext("2d");
-    this.canvas.lineWidth = 6;
-    this.canvas.strokeStyle = "#4C4CEE";
-    this.element.style.fontFamily = "Manjari"
-  }
-
-  reset() {
-    this.choice = null;
-    this.match = false
-    this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = ''
-    running = true;
-    player = "x";
-    document.getElementById("winner").innerHTML = "<br>";
-    document.getElementById("reset-button").disabled = true;
-    this.canvas.clearRect(0,0,370,370);
-    document.getElementById("myCanvas").hidden = true;
-
-  }
-
-  checkForWinner(){
-    if (turn >= 4) {
-      var x;
-
-      if (this.choice == "x") {
-        this.canvas.strokeStyle = "#FF0000";
-      }
-      else {
-        this.canvas.strokeStyle = "#0000FF";
-      }
-
-      for (var i = 0;i<3; i++) {
-        x = i*3
-        if (boardSquares[x].choice ==
-          this.choice && boardSquares[x+1].choice == this.choice && boardSquares[x+2].choice == this.choice) {
-          this.match = true;
-          this.canvas.beginPath();
-          this.canvas.moveTo(41, 62+(i)*(122));
-          this.canvas.lineTo(329, 62+(i)*(122));
-          this.canvas.stroke();
-        }
-      }
-
-      for (var i = 0;i<3;i++) {
-        if (boardSquares[i].choice ==
-          this.choice && boardSquares[i+3].choice == this.choice && boardSquares[i+6].choice == this.choice) {
-          this.match = true;
-          this.canvas.beginPath();
-          this.canvas.moveTo(62+(i)*(122), 41);
-          this.canvas.lineTo(62+(i)*(122), 329);
-          this.canvas.stroke();
-        }
-      }
-
-      if (boardSquares[0].choice == this.choice && boardSquares[4].choice == this.choice && boardSquares[8].choice == this.choice){
-        this.match = true;
-        this.canvas.beginPath();
-        this.canvas.moveTo(41, 41);
-        this.canvas.lineTo(329, 329);
-        this.canvas.stroke();
-      }
-      else if (boardSquares[2].choice == this.choice && boardSquares[4].choice == this.choice && boardSquares[6].choice == this.choice){
-        this.match = true;
-        this.canvas.beginPath();
-        this.canvas.moveTo(329, 41);
-        this.canvas.lineTo(41, 329);
-        this.canvas.stroke();
-      }
-
-      if (this.match) {
-        document.getElementById("myCanvas").hidden = false;
-        document.getElementById("winner").innerHTML = "Winner is: " + player.toUpperCase();
-        document.getElementById("reset-button").disabled = false;
-        running = false;
-      }
-    }
-  }
-
-  checkForDraw() {
-    if (turn == 8 && this.match == false) {
-      document.getElementById("winner").innerHTML = "The game ended in a draw";
-      document.getElementById("reset-button").disabled = false;
-      running = false;
-    }
-  }
-
-  handleEvent(event) {
-    switch (event.type) {
-      case "click":
-      if (running) {
-        if (player == "x" && this.choice == null) {
-          console.log(this.element.querySelector(".face-container").querySelector(".facedown"))
-          this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '<p>X</p>'
-          this.element.querySelector(".face-container").querySelector(".facedown").style.color = "#E93636"
-          this.choice = "x";
-          this.checkForWinner();
-          this.checkForDraw();
-          player = "o";
-          turn ++;
-        }
-        else if (player == "o" && this.choice == null) {
-          this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '<p>O</p>'
-          this.element.querySelector(".face-container").querySelector(".facedown").style.color = "#4C4CEE"
-          this.choice = "o";
-          this.checkForWinner();
-          this.checkForDraw();
-          player = "x"
-          turn ++;
-        }
-      }
-    }
-  }
-}
-
-resetButton.addEventListener('click', () => {
-  resetGame();
-  turn = 0;
-});
-
+//Creates a canvas sizing it depending on the window size
+//returns the canvas HTML as well as the canvas object
 function createCanvas() {
   var c = document.getElementById("myCanvas");
-  c.width = 370;
-  c.height = 370;
-  return c
+  if (window.innerWidth < 400){
+    c.width = 300;
+    c.height = 300;
+  }
+  else{
+    c.width = 370;
+    c.height = 370;
+  }
+  canvas = c.getContext("2d");
+  canvas.lineWidth = 6;
+  console.log(c,canvas)
+  return [c, canvas]
 }
 
+//calls the generate squares function, creates a canvas, pushes each square to the boardsuares array, returns the canvas
 function setupGame(){
   generateHTMLBoardSquares();
-  var c = createCanvas();
+  var [c,canvas] = createCanvas();
 
 
   const squareElements = document.getElementsByClassName("board-square");
 
   for (var i = 0; i<squareElements.length; i++) {
     const element = squareElements[i];
-    const square = new BoardSquares(element,c);
+    const square = new BoardSquares(element);
     boardSquares.push(square);
   }
-
-function GetElementInsideContainer(containerID, childID) {
-  var elm = document.getElementById(childID);
-  var parent = elm ? elm.parentNode : {};
-  return (parent.id && parent.id === containerID) ? elm : {};
+  return [c,canvas]
 }
 
-}
+/*
+//Resizes the size of the canvas whenever the window width become smaller than 400px
+//Whenever the canvas size changes, the canvas clears
+setInterval(function(){
+  window.onresize = function(){
+    if (window.innerWidth < 400){
+      c.width = 300;
+      c.height = 300;
+    }
+    else{
+      c.width = 370;
+      c.height = 370;
+    }
+  }
+}, 100);
+*/
 
-function resetGame() {
-  boardSquares.forEach((square)=> {
-    square.reset()
-  });
-}
-
-setupGame();
+//starts the game, also allows the canvas HTML and the canvas object to be a global variable
+var [c,canvas] = setupGame();
