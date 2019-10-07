17a42d2e1956635418c5b0233e56ef30a3325387
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-10-07 14:43:30 -0700

    Cleaned up and organized code

--- CompletedProjects/tic-tac-toe/js/main.js
+++ CompletedProjects/tic-tac-toe/js/main.js
@@ -21,7 +21,57 @@ var turn = 0
 //Want to know whenever the game is running
 var running = true
 
-//Each square on the board is an object of the BoardSquare class
+
+// Generates squares for the board game
+function setupGame() {
+  generateHTMLBoardSquares();
+
+  const squareElements = document.getElementsByClassName("board-square");
+
+  for (var i = 0; i < squareElements.length; i++) {
+    const element = squareElements[i];
+    const square = new BoardSquares(element);
+    boardSquares.push(square);
+  }
+}
+
+
+//––––––––– TODO: Function for adding all the squares onto the Board ––––––––
+//Adds boardsquare divs as well as the canvas div following all squares
+function generateHTMLBoardSquares(){
+  const boardElement = document.getElementById('gameboard');
+  sqrHtml = ''
+  for (var i = 0; i<9;i++) {
+    sqrHtml += `<div class="col-4 board-square ">
+                    <div class="face-container">
+                      <div class="facedown text-center">
+                      </div>
+                    </div>
+                  </div>`
+  }
+  sqrHtml += `<canvas id="myCanvas" hidden></canvas>`
+  boardElement.innerHTML = sqrHtml;
+}
+
+// Called when reset button is clicked
+// Resets all the board squares of the game
+function resetGame() {
+  boardSquares.forEach((square)=> {
+    square.reset()
+  });
+}
+
+// event listener for the reset button
+document.getElementById("reset-button").addEventListener('click', () => {
+  resetGame();
+  turn = 0;
+});
+
+
+/*
+Each square on the board is an object of the BoardSquare class
+This will allow us to manipulate the objects whenever they are clicked
+*/
 class BoardSquares {
   constructor(element) {
     this.element = element;
@@ -31,33 +81,16 @@ class BoardSquares {
     this.element.style.fontFamily = "Manjari";
   }
 
-  //Restarts the game
-  reset() {
-    this.choice = null;
-    this.match = false
-    this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '';
-    this.element.style.backgroundColor = "#fff";
-    running = true;
-    player = "x";
-    document.getElementById("winner").innerHTML = "<br>";
-    document.getElementById("reset-button").disabled = true;
-    document.getElementById("myCanvas").hidden = true;
-  }
-
-  //Checks every single possiblilty for a win whenever there have been more then 4 turn taken
-  //Calls drawLine function to draw a line
+  /*
+  Checks every single possiblilty for a win whenever there have been more then 4 turn taken
+  Calls drawLine function to draw a line
+  */
   checkForWinner(){
     if (turn >= 4) {
       var x;
 
       // Check rows and columns
-      this.winSides();
-
-      // Diagonal top left to bottom right
-      this.winDiagonal(0,4,8);
-
-      // Diagonal top right to bottom left
-      this.winDiagonal(2,4,6);
+      this.checkSides()
 
       if (this.match) {
         document.getElementById("winner").innerHTML = "Winner is: " + player.toUpperCase();
@@ -67,47 +100,36 @@ class BoardSquares {
     }
   }
 
-  winSides(){
-    for (var y = 0;y<3; y++) {
-        let x = y*3;
+  /* TODO:
+    Check all the sides (i.e. Horizontal, vertical, diagonal)
+    to see if any of the players won already
+  */
+  checkSides(){
+    let sides = [[0,3,6], [1,4,7], [2,5,8], // Vertical Check
+                 [0,1,2], [3,4,5], [6,7,8], // Horizontal check
+                 [0,4,8], [2,4,6]]          // Diagonal Check
 
-        // Check vertically
-        let y1 = boardSquares[y].choice;
-        let y2 = boardSquares[y+3].choice;
-        let y3 = boardSquares[y+6].choice;
+    // Check vertical sides
+    for (var i = 0; i < sides.length; i++) {
 
-        // Check horizontally
-        let x1 = boardSquares[x].choice;
-        let x2 = boardSquares[x+1].choice;
-        let x3 = boardSquares[x+2].choice;
-        
-        if (y1 == y2 && y1 == y3){
-          this.match = true;
-          this.changeSquareWin([y,y+3,y+6]);
-          return
-        }
-        
-        if (x1 == x2 && x1 == x3){
-          this.match = true;
-          this.changeSquareWin([x,x+1,x+2]);
-          return
-        }
+        let sqrs = sides[i]
+
+        let s1 = boardSquares[sqrs[0]].choice
+        let s2 = boardSquares[sqrs[1]].choice
+        let s3 = boardSquares[sqrs[2]].choice
+
+        if (s1 != null && s1 == s2 && s1 == s3) {
+            this.match = true;
+            this.changeSquareWin(sqrs);
+            return
+          }
       }
+
   }
 
-  winDiagonal(sq1,sq2,sq3) {
-    let s1 = boardSquares[sq1].choice;
-    let s2 = boardSquares[sq2].choice;
-    let s3 = boardSquares[sq3].choice;
-    if (s1 == s2 && s1 == s3){
-        this.match = true;
-        this.changeSquareWin([sq1,sq2,sq3]);
-        return
-      }
-    
-  }
-
-  //Checks whether the game ended in a draw
+  /* TODO: Check for Draw
+  Checks whether the game ended in a draw
+  */
   checkForDraw() {
     if (turn == 8 && this.match == false) {
       document.getElementById("winner").innerHTML = "The game ended in a draw";
@@ -140,7 +162,7 @@ class BoardSquares {
     });
   }
 
-  //Changes the text on the square
+  // Changes the text on the square
   selectSquare(currPlayer, color,nxtPlayer ){
     this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '<p>'+currPlayer.toUpperCase()+'</p>';
     this.element.querySelector(".face-container").querySelector(".facedown").style.color = color;
@@ -151,56 +173,21 @@ class BoardSquares {
     turn ++;
   }
 
-  //Converts fraction to pixel
-  fracToPixel(frac){
-    return frac*c.width
+  //Restarts the game
+  reset() {
+    this.choice = null;
+    this.match = false
+    this.element.querySelector(".face-container").querySelector(".facedown").innerHTML = '';
+    this.element.style.backgroundColor = "#fff";
+    running = true;
+    player = "x";
+    document.getElementById("winner").innerHTML = "<br>";
+    document.getElementById("reset-button").disabled = true;
+    document.getElementById("myCanvas").hidden = true;
   }
+
 }
 
-//event listener for the reset button
-document.getElementById("reset-button").addEventListener('click', () => {
-  resetGame();
-  turn = 0;
-});
-
-//called on by event listener for reset button
-//iterates thorought boardSquares array resetting each square one at a time
-function resetGame() {
-  boardSquares.forEach((square)=> {
-    square.reset()
-  });
-}
-
-//––––––––– TODO: Function for adding all the squares onto the Board ––––––––
-//Adds boardsquare divs as well as the canvas div following all squares
-function generateHTMLBoardSquares(){
-  const boardElement = document.getElementById('gameboard');
-  sqrHtml = ''
-  for (var i = 0; i<9;i++) {
-    sqrHtml += `<div class="col-4 board-square ">
-                    <div class="face-container">
-                      <div class="facedown text-center">
-                      </div>
-                    </div>
-                  </div>`
-  }
-  sqrHtml += `<canvas id="myCanvas" hidden></canvas>`
-  boardElement.innerHTML = sqrHtml;
-}
-
-
-//calls the generate squares function, creates a canvas, pushes each square to the boardsuares array, returns the canvas
-function setupGame() {
-  generateHTMLBoardSquares();
-
-  const squareElements = document.getElementsByClassName("board-square");
-
-  for (var i = 0; i < squareElements.length; i++) {
-    const element = squareElements[i];
-    const square = new BoardSquares(element);
-    boardSquares.push(square);
-  }
-}
 
 // Launch game
 setupGame();
