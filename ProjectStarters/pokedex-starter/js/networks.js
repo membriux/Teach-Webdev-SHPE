7b82940b9b8e72b8186c0e07bb8a7834bb14c943
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-10-11 14:58:18 -0700

    Added project starter files

---
+++ ProjectStarters/pokedex-starter/js/networks.js
@@ -0,0 +1,26 @@
+class Networks {
+
+    // ––––––––– TODO: Get JSON data from internet
+    static getJSON(callback) {
+
+    }
+
+
+    // ––––––––– TODO: Retrieve all pokemon data and add to HTML
+    static fetchAllPokemon() {
+        return
+    }
+
+    // ––––––––– TODO: Fetch specific pokemon that was selected and display
+    //                 it's information
+    static fetchPokemonDetails(pokemonId) {
+        return 
+    }
+}
+
+
+// After Data has been loaded, stop showing the loading animation
+function hideLoading() {
+    let loading = document.getElementsByClassName('loading')[0];
+    loading.classList.add('hide-loading');
+}
