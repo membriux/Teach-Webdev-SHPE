059dcd1ea021cba1610fe76eae266b7effd185e9
guilleeh <guilleeh@uci.edu>
2019-10-18 17:55:54 -0700

    Basic layout of github user created

--- CompletedProjects/github-finder/js/networks.js
+++ CompletedProjects/github-finder/js/networks.js
@@ -1,13 +1,17 @@
 class Network {
     // STEP 1
     // Send GET request to retreive github users
-    static searchUsers(username) {
-        let result = null;
-        $.get(`https://api.github.com/search/users?q=${username}`, function(
-            data
-        ) {
+    static searchUsers(query) {
+        $.get(`https://api.github.com/search/users?q=${query}`, function(data) {
             //If success, populate html
             displayUsers(data.items);
+        });
+    }
+
+    static getUser(username) {
+        $.get(`https://api.github.com/users/${username}`, function(data) {
+            // if success, populate user html
+            displaySingleUser(data.items);
         });
     }
 }
