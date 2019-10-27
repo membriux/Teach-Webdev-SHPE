f66941e2560ce21096786b342cbfd54267f46830
guilleeh <guilleeh@uci.edu>
2019-10-26 18:04:05 -0700

    cleanup

--- CompletedProjects/mongo-chat/dbconnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
-
-const url = "mongodb+srv://students:$hpEWebD3v@cluster0-p2gnl.mongodb.net/test?retryWrites=true&w=majority"
-
-const connect = mongoose.connect(url, {useNewUrlParser: true});
-
-module.exports = connect;
