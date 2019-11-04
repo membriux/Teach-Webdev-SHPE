937286cdd37621e0cda658eeed99e6911105f64b
Osvaldo Vallejo <vallejoosvaldo12@yahoo.com>
2019-11-03 23:19:08 -0800

    commit

--- CompletedProjects/Ecommerce/routes/index.js
+++ CompletedProjects/Ecommerce/routes/index.js
@@ -47,15 +47,7 @@ router.post('/login', (req, res, next) => {
 router.post('/login', (req, res, next) => {
   res.redirect('items/index');
 });
-// exports.requireLogin = (req, res, next) => {
-//   if(req.session && req.session.userId) {
-//     return next();
-//   } else {
-//     let err = new Error('You must log in to view this page');
-//     err.status = 401;
-//     return res.redirect('/login');
-//   }
-// }
+
 
 
 
