84c54acd9f831585c47da8b2a07457ced5c9da4f
Osvaldo Vallejo <vallejoosvaldo12@yahoo.com>
2019-11-03 22:27:47 -0800

    commit

--- CompletedProjects/Ecommerce/routes/index.js
+++ CompletedProjects/Ecommerce/routes/index.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const Item = require('../models/item');
+const User = require('../models/user');
 
 
 // ––––––– STEP 2: Route user to main page
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Item.find({},  function(err, items) {
-    if(err) {
-      console.error(err);
-    } else {
-      res.render('items/index', { items: items });
-    }
-  });
+  res.redirect('/login');
 });
 
 // Login User
@@ -23,21 +18,37 @@ router.get('/login', (req, res, next) => {
 
 // POST login
 router.post('/login', (req, res, next) => {
-  User.authenticate(req.body.username, req.body.password, (err, user) => {
-    if (err || !user) {
-      const next_error = new Error("Username or password incorrect");
-      next_error.status = 401;
 
-      return next(next_error);
-    } else {
-      req.session.userId = user._id;
+  // User Clicks on "Login"
+  if('login' in req.body) {
+    User.authenticate(req.body.username, req.body.password, (err, user) => {
+      if (err || !user) {
+        const next_error = new Error("Username or password incorrect");
+        next_error.status = 401;
 
-      return res.redirect('/') ;
-    }
-  });
+        return next(next_error);
+      } else {
+        return res.redirect('items/');
+        }
+    });
+
+  }
+
+  // User clicks "Signup"
+  else if('signup' in req.body) {
+    const user = new User(req.body);
+
+    user.save(function(err, user) {
+      if(err) console.log(err);
+      return res.redirect('items/');
+    });
+  }
+
 });
 
-
+router.post('/login', (req, res, next) => {
+  res.redirect('items/index');
+});
 // exports.requireLogin = (req, res, next) => {
 //   if(req.session && req.session.userId) {
 //     return next();
